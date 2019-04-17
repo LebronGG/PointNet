8bd8f747a2bbcbb45375823c95dc63f447ae8ad1
LebronGG <38722951+LebronGG@users.noreply.github.com>
2019-04-17 16:12:16 +0800

    Update eval_iou_accuracy.py

--- sem_seg/eval_iou_accuracy.py
+++ sem_seg/eval_iou_accuracy.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-pred_data_label_filenames = [line.rstrip() for line in open('all_pred_data_label_filelist.txt')]
+i=1
+pred_data_label_filenames = [line.rstrip() for line in open('log{}/output_filelist.txt'.format(i))]
 gt_label_filenames = [f.rstrip('_pred\.txt') + '_gt.txt' for f in pred_data_label_filenames]
 num_room = len(gt_label_filenames)
 
@@ -14,7 +14,7 @@ for i in range(num_room):
     pred_label = data_label[:,-1]
     gt_label = np.loadtxt(gt_label_filenames[i])
     print(gt_label.shape)
-    for j in xrange(gt_label.shape[0]):
+    for j in range(gt_label.shape[0]):
         gt_l = int(gt_label[j])
         pred_l = int(pred_label[j])
         gt_classes[gt_l] += 1
@@ -26,14 +26,13 @@ print(gt_classes)
 print(positive_classes)
 print(true_positive_classes)
 
-
 print('Overall accuracy: {0}'.format(sum(true_positive_classes)/float(sum(positive_classes))))
 
-print 'IoU:'
+print ('IoU:')
 iou_list = []
 for i in range(13):
     iou = true_positive_classes[i]/float(gt_classes[i]+positive_classes[i]-true_positive_classes[i]) 
-    print(iou)
+    print('{}:{}'.format(i,iou))
     iou_list.append(iou)
 
-print(sum(iou_list)/13.0)
+print('mIou:',sum(iou_list)/13.0)
